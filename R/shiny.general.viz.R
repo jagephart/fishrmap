8bca96a0874eceacfd8b08f235e6d8681f0678e7
Sigfried Gold <sigfried@sigfried.org>
2018-08-16 09:00:31 -0400

    playing with modularizing shiny...maybe belongs in package?

---
+++ R/shiny.general.viz.R
@@ -0,0 +1,66 @@
+dimAssignmentInput <- function(id, 
+                               label = "Data to Display Dimension Assignments") {
+  # Create a namespace function using the provided id
+  ns <- NS(id)
+  
+  tagList(
+    fileInput(ns("file"), label),
+    checkboxInput(ns("heading"), "Has heading"),
+    selectInput(ns("quote"), "Quote", c(
+      "None" = "",
+      "Double quote" = "\"",
+      "Single quote" = "'"
+    ))
+  )
+}
+
+dimAssignmentServer <- function(input, output, session, stringsAsFactors) {
+  # The selected file, if any
+  userFile <- reactive({
+    # If no file is selected, don't do anything
+    validate(need(input$file, message = FALSE))
+    input$file
+  })
+  
+  # The user's data, parsed into a data frame
+  dataframe <- reactive({
+    read.csv(userFile()$datapath,
+             header = input$heading,
+             quote = input$quote,
+             stringsAsFactors = stringsAsFactors)
+  })
+  
+  # We can run observers in here if we want to
+  observe({
+    msg <- sprintf("File %s was uploaded", userFile()$name)
+    cat(msg, "\n")
+  })
+  
+  # Return the reactive that yields the data frame
+  return(dataframe)
+}
+
+
+library(shiny)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      dimAssignmentInput("datafile", "User data (.csv format)")
+    ),
+    mainPanel(
+      dataTableOutput("table")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  datafile <- callModule(dimAssignmentServer, "datafile",
+                         stringsAsFactors = FALSE)
+  
+  output$table <- renderDataTable({
+    datafile()
+  })
+}
+
+shinyApp(ui, server)
